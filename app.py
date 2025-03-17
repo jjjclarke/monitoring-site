0cac5da794fd26792a51322aaa76783bf53f4256
James Clarke <164679524+jjjclarke@users.noreply.github.com>
2025-03-17 07:57:55 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, Response, render_template
+import live_feed
+
+app = Flask(__name__)
+
+# Initialise camera
+live_feed.init_camera()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/live_feed')
+def live_feed():
+    # Use the gen_frames function from the live_feed module
+    return Response(live_feed.gen_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
